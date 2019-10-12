1de13d3bb4dec92abcb814f96d992359ace13f52
MaxBlesch <s6mables@uni-bonn.de>
2019-10-12 15:22:46 +0200

    F-strings.

--- data/data_processing.py
+++ data/data_processing.py
@@ -25,8 +25,10 @@ def data_processing(init_dict):
     dirname = os.path.dirname(__file__)
     df_end = pd.DataFrame()
     df_pool = pd.DataFrame()
-    for group in init_dict["groups"].split(","):
-        df = pd.read_pickle(dirname + "/pkl/group_data/" + group + ".pkl")
+    groups = init_dict["groups"]
+    binsize = init_dict["binsize"]
+    for group in groups.split(","):
+        df = pd.read_pickle(f"{dirname}/pkl/group_data/{group}.pkl")
         repl = pd.Series(index=df.index, data=0, dtype=int)
         for i in df.columns.values[11:]:
             df2 = df[["Bus_ID", i]]
@@ -57,18 +59,11 @@ def data_processing(init_dict):
         num_bus = len(df_end["Bus_ID"].unique())
         num_periods = df_end.shape[0] / num_bus
         df_end["period"] = np.arange(num_periods).repeat(num_bus).astype(int)
-        df_end[["state"]] = (df_end[["state"]] / init_dict["binsize"]).astype(int)
+        df_end[["state"]] = (df_end[["state"]] / binsize).astype(int)
         df_end.sort_values(["Bus_ID", "period"], inplace=True)
         df_end.reset_index(drop=True, inplace=True)
         df_pool = pd.concat([df_pool, df_end], axis=0)
     df_pool.reset_index(drop=True, inplace=True)
     os.makedirs(dirname + "/pkl/replication_data", exist_ok=True)
-    df_pool.to_pickle(
-        dirname
-        + "/pkl/replication_data/rep_"
-        + init_dict["groups"]
-        + "_"
-        + str(init_dict["binsize"])
-        + ".pkl"
-    )
+    df_pool.to_pickle(f"{dirname}/pkl/replication_data/rep_{groups}_{binsize}.pkl")
     return df_pool
